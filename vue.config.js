81046e5d6a34d91d89ae6491bf04c1dad9a6d804
weilantian <xiaolanlub@outlook.com>
2021-04-18 20:11:10 +0800

    update vue config file

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
 // vue.config.js
+const forSharing = true
 
+const path = forSharing? './':'/~s3882916/'
 /**
  * @type {import('@vue/cli-service').ProjectOptions}
  */
  module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/~s3882916/': '/'
+    ? path: '/'
   }
